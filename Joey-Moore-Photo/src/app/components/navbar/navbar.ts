b14b5a81fbf7d7a24170df4be35c52bb2ed1e115
JoeyM25 <morgeo18@gmail.com>
2025-10-21 11:56:27 -0500

    Finished dialog for the book button. Also changed some style things.

--- Joey-Moore-Photo/src/app/components/navbar/navbar.ts
+++ Joey-Moore-Photo/src/app/components/navbar/navbar.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton, MatIconButton, MatButtonModule} from '@angular/material/button';
+import { MatMenu } from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatToolbar, MatIcon, MatButton, MatIconButton, MatMenu, MatMenuModule, MatIconModule, MatButtonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
--- Joey-Moore-Photo/src/app/pages/home/home.ts
+++ Joey-Moore-Photo/src/app/pages/home/home.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, model, signal } from '@angular/core';
 import { ImgCar } from '../../components/img-car/img-car';
+import { MatAnchor } from "@angular/material/button";
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose } from '@angular/material/dialog';
+import { inject } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
+export interface DialogData {
+  name: string;
+  animal: string;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [ImgCar],
+  imports: [ImgCar, MatAnchor, MatButtonModule, MatFormFieldModule, MatInputModule, FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  readonly name = signal('');
+  readonly animal = model('');
+  readonly dialog = inject(MatDialog);
 
+  openBookDialog() {
+    const dialogRef = this.dialog.open(bookdialog, {
+      data: {name: this.name(), animal: this.animal()},
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      if (result != undefined) {
+        console.log("ballz")
+      }
+    });
+  }
+}
+
+@Component({
+  selector: 'bookdialog',
+  templateUrl: './bookdialog.html',
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
+})
+export class bookdialog {
+  readonly dialogRef = inject(MatDialogRef<bookdialog>);
+  readonly data = inject<DialogData>(MAT_DIALOG_DATA);
+  readonly animal = model(this.data.animal);
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
