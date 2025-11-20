5f0023e1a1129566d2f7513bd6ac076a60b59629
JoeyM25 <morgeo18@gmail.com>
2025-11-20 13:07:15 -0600

    Attepmt to deploy

---
+++ Joey-Moore-Photo/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAcDIlRTYRURSR60yJyqt6iCM0Ibj3eKd0",
+    authDomain: "joey-moore-photography.firebaseapp.com",
+    projectId: "joey-moore-photography",
+    storageBucket: "joey-moore-photography.firebasestorage.app",
+    messagingSenderId: "697788136261",
+    appId: "1:697788136261:web:9894edac77c05cf886dc2c",
+    measurementId: "G-VSH388P69Y"
+  }
+};
